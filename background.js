b93bba78110f86b034eded925c928f4bf79ebf1d
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-04-21 13:24:19 +0800

    Updated ContentScripts and Checker if Button have Rendered

--- background.js
+++ background.js
@@ -1,20 +1,21 @@
 // Listen to updates in our tab system
 
-chrome.tabs.onUpdated.addListener((tabId, tab)=>{
-    //grab tab name and url
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  //grab tab name and url
 
-    console.log("this is current tab", tab.url)
-    console.log("this is current tab title", tab.title)
+  console.log("this is current tab", tab.url);
+  console.log("this is current tab title", tab.title);
 
-    
-if(tab.url && tab.url.includes('youtube.com/watch')){
-    const queryParameters= tab.url.split('?')[1];
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
-    console.log("this is the video id", urlParameters)
+    console.log("this is the video id", urlParameters);
     //send a mesage to the content script that there is a new video loaded
     chrome.tabs.sendMessage(tabId, {
-        type:"NEW",
-        videoId: urlParameters.get('v')
-    })
-}
-})
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    });
+
+    
+  }
+});
--- contentScript.js
+++ contentScript.js
@@ -8,4 +8,20 @@
       newVideoLoaded();
     }
   });
+
+  const newVideoLoaded = () => {
+    const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+      console.log("bookmark extists ?",bookmarkBtnExists);
+
+
+      if(!bookmarkBtnExists){
+        const bookmarkBtn = document.createElement("img");
+        bookmarkBtn.src = chrome.runtime.getURL("images/bookmark.png"); 
+        bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+        bookmarkBtn.title = "Bookmark this video";
+      }
+  };
+
 })();
+
+
